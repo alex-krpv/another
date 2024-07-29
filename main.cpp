1bab0e8a673d64bbf08126814aca03a596fe0084
Alexey Karpov <karpov_official@mail.ru>
2024-07-29 16:53:20 +0300

    s11_json1

--- main.cpp
+++ main.cpp
@@ -1,116 +1,39 @@
-#include "canvas.h"
-#include "shapes.h"
-
+#include "json_builder.h"
 #include <iostream>
-#include <cassert>
-#include <sstream>
 
-std::unique_ptr<Texture> MakeTextureCow() {
-	Image image = { R"(^__^            )",  //
-				   R"((oo)\_______    )",  //
-				   R"((__)\       )\/\)",  //
-				   R"(    ||----w |   )",  //
-				   R"(    ||     ||   )" };
-	return std::make_unique<Texture>(move(image));
-}
 
-//делает сплошную текстуру из пикселей заданного "цвета" pixel
-std::unique_ptr<Texture> MakeTextureSolid(Size size, char pixel) {
-	Image image(size.height, std::string(size.width, pixel));
-	return std::make_unique<Texture>(move(image));
-}
-//делает текстуру из пикселей с чередующимися "цветами"
-std::unique_ptr<Texture> MakeTextureCheckers(Size size, char pixel1, char pixel2) {
-	Image image(size.height, std::string(size.width, pixel1));
-
-	for (int i = 0; i < size.height; ++i) {
-		for (int j = 0; j < size.width; ++j) {
-			if ((i + j) % 2 != 0) {
-				image[i][j] = pixel2;
-			}
-		}
-	}
-
-	return std::make_unique<Texture>(move(image));
-}
-
-void TestCpp() {
-	Canvas canvas(Size{ 77, 17 });
-
-	// Буква "C" как разность двух эллипсов, один из которых нарисован цветом фона
-	canvas.AddShape(ShapeType::ELLIPSE, { 2, 1 }, { 30, 15 },
-		MakeTextureCheckers({ 100, 100 }, 'c', 'C'));
-	canvas.AddShape(ShapeType::ELLIPSE, { 8, 4 }, { 30, 9 }, MakeTextureSolid({ 100, 100 }, ' '));
-
-	// Горизонтальные чёрточки плюсов
-	auto h1 = canvas.AddShape(ShapeType::RECTANGLE, { 54, 7 }, { 22, 3 },
-		MakeTextureSolid({ 100, 100 }, '+'));
-	canvas.DuplicateShape(h1, { 30, 7 });
-
-	// Вертикальные чёрточки плюсов
-	auto v1 = canvas.DuplicateShape(h1, { 62, 3 });
-	canvas.ResizeShape(v1, { 6, 11 });
-	canvas.DuplicateShape(v1, { 38, 3 });
-
-	std::stringstream output;
-	canvas.Print(output);
-
-	const auto answer
-		= "###############################################################################\n"
-		"#                                                                             #\n"
-		"#            cCcCcCcCcC                                                       #\n"
-		"#        CcCcCcCcCcCcCcCcCc                                                   #\n"
-		"#      cCcCcCcCcCcCcCcCcCcCcC          ++++++                  ++++++         #\n"
-		"#    CcCcCcCcCcCc                      ++++++                  ++++++         #\n"
-		"#   CcCcCcCcC                          ++++++                  ++++++         #\n"
-		"#   cCcCcCc                            ++++++                  ++++++         #\n"
-		"#  cCcCcC                      ++++++++++++++++++++++  ++++++++++++++++++++++ #\n"
-		"#  CcCcCc                      ++++++++++++++++++++++  ++++++++++++++++++++++ #\n"
-		"#  cCcCcC                      ++++++++++++++++++++++  ++++++++++++++++++++++ #\n"
-		"#   cCcCcCc                            ++++++                  ++++++         #\n"
-		"#   CcCcCcCcC                          ++++++                  ++++++         #\n"
-		"#    CcCcCcCcCcCc                      ++++++                  ++++++         #\n"
-		"#      cCcCcCcCcCcCcCcCcCcCcC          ++++++                  ++++++         #\n"
-		"#        CcCcCcCcCcCcCcCcCc                                                   #\n"
-		"#            cCcCcCcCcC                                                       #\n"
-		"#                                                                             #\n"
-		"###############################################################################\n";
-
-	assert(answer == output.str());
-}
-
-void TestCow() {
-	Canvas canvas{ {18, 5} };
-
-	canvas.AddShape(ShapeType::RECTANGLE, { 1, 0 }, { 16, 5 }, MakeTextureCow());
-
-	std::stringstream output;
-	canvas.Print(output);
-	//canvas.Print(std::cout);
-	// clang-format off
-	// Здесь уместно использовать сырые литералы, т.к. в текстуре есть символы '\'
-	const auto answer =
-		R"(####################)""\n"
-		R"(# ^__^             #)""\n"
-		R"(# (oo)\_______     #)""\n"
-		R"(# (__)\       )\/\ #)""\n"
-		R"(#     ||----w |    #)""\n"
-		R"(#     ||     ||    #)""\n"
-		R"(####################)""\n";
-	// clang-format on
-
-	assert(answer == output.str());
-}
-
-void TestNegativeCoords() {
-	Canvas canvas{ {18, 5} };
-	canvas.AddShape(ShapeType::RECTANGLE, { 0, 0 }, { 18, 5 }, MakeTextureSolid({ 100, 100 }, '*'));
-	canvas.AddShape(ShapeType::RECTANGLE, { 9, 2 }, { 2, 2 }, MakeTextureSolid({ 1, 1 }, '+'));
-	canvas.Print(std::cout);
-}
+using namespace std;
 
 int main() {
-	//TestCow();
-	//TestCpp();
-	TestNegativeCoords();
+    json::Print(
+        json::Document{
+            json::Builder{}
+            .StartDict()
+                .Key("key1"s).Value(123)
+                .Key("key2"s).Value("value2"s)
+                .Key("key3"s).StartArray()
+                    .Value(456)
+                    .StartDict().EndDict()
+                    .StartDict()
+                        .Key(""s)
+                        .Value(nullptr)
+                    .EndDict()
+                    .Value(""s)
+                .EndArray()
+            .EndDict()
+            .Build()
+        },
+        cout
+    );
+    cout << endl;
+    
+    json::Print(
+        json::Document{
+            json::Builder{}
+            .Value("just a string"s)
+            .Build()
+        },
+        cout
+    );
+    cout << endl;
 }
