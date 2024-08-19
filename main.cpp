53fa8580fef6ad840e6074474fd1e91187de1493
Alexey Karpov <karpov_official@mail.ru>
2024-08-19 16:54:26 +0300

    Спринт 12. budget3-begin

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include "budget_manager.h"
 #include "parser.h"
 
 #include <iostream>
-#include<cassert>
-#include <sstream>
-#include <string_view>
 
 using namespace std;
 
 void ParseAndProcessQuery(BudgetManager& manager, string_view line) {
-    Request request(manager);
-    request.ParseLine(line);
-    request.ApplyCommand(&std::cout);
+    auto query = ParseQuery(line);
+
+    if (!query) {
+        return;
+    }
+
+    query->ProcessAndPrint(manager, cout);
 }
 
 int ReadNumberOnLine(istream& input) {
@@ -19,55 +19,9 @@ int ReadNumberOnLine(istream& input) {
     std::getline(input, line);
     return std::stoi(line);
 }
-namespace tests {
-    inline const double EPSILON = 1e-6;
-    void ManagerEarningsSetGet() {
-        //istringstream in("2000 - 01 - 02 2000 - 01 - 06 20");
-        {
-            BudgetManager manager;
-            manager.SetEarnings(Date{ 2000,1,2 }, Date{ 2000,1,6 }, 20);
-            assert(manager.GetEarnings(Date{ 2000,1,2 }, Date{ 2000,1,6 }) == 20);
-            assert(manager.GetEarnings(Date{ 2000,1,2 }, Date{ 2000,1,5 }) == 16);
-            manager.PayTax(Date{ 2000,1,2 }, Date{ 2000,1,3 },13);
-            assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2001,1,1 }) - 18.96) < EPSILON);
-            manager.SetEarnings(Date{ 2000,1,3 }, Date{ 2000,1,3 }, 10);
-            assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2001,1,1 }) - 28.96) < EPSILON);
-            cout << "Tests1 passed" << endl;
-        }
-        //тестирование граничных значений дат
-        {
-            BudgetManager manager;
-            manager.SetEarnings(Date{ 2000,1,1 }, Date{ 2000,1,1 }, 20);
-            assert(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2099,12,31 }) == 20);
-            manager.PayTax(Date{ 2000,1,1 }, Date{ 2000,1,3 },13);
-            assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2099,12,13 }) - 17.4) < EPSILON);
-            manager.SetEarnings(Date{ 2099,12,31 }, Date{ 2099,12,31 }, 10);
-            assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2099,12,31 }) - 27.4) < EPSILON);
-            manager.PayTax(Date{ 2099,12,31 }, Date{ 2099,12,31 }, 13);
-            assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2099,12,31 }) - 26.1) < EPSILON);
-            cout << "Tests2 passed" << endl;
-        }
-        
 
-    }
-    void RequestApply() {
-        BudgetManager manager;
-        ParseAndProcessQuery(manager, "Earn 2000-01-02 2000-01-06 20"s);
-        assert(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2099,12,31 }) == 20);
-        ParseAndProcessQuery(manager, "ComputeIncome 2000-01-02 2000-01-06"s);
-        ParseAndProcessQuery(manager, "PayTax 2000-01-02 2000-01-03 13"s);
-        assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2001,1,1 }) - 18.96) < EPSILON);
-        ParseAndProcessQuery(manager, "Spend 2000-12-30 2001-01-02 14"s);
-        assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2001,1,1 }) - 8.46) < EPSILON);
-        ParseAndProcessQuery(manager, "PayTax 2000-12-30 2000-12-30 13"s);
-        assert(std::abs(manager.GetEarnings(Date{ 2000,1,1 }, Date{ 2001,1,1 }) - 8.46) < EPSILON);
-
-    }
-}
 int main() {
-    tests::ManagerEarningsSetGet();
-    tests::RequestApply();
-    /*BudgetManager manager;
+    BudgetManager manager;
 
     const int query_count = ReadNumberOnLine(cin);
 
@@ -75,5 +29,5 @@ int main() {
         std::string line;
         std::getline(cin, line);
         ParseAndProcessQuery(manager, line);
-    }*/
+    }
 }
--- parser.cpp
+++ parser.cpp
@@ -1,107 +1,77 @@
 #include "parser.h"
 
-/**
- * Удаляет пробелы в начале и конце строки
- */
-std::string_view Trim(std::string_view string) {
-    const auto start = string.find_first_not_of(' ');
-    if (start == string.npos) {
-        return {};
+using namespace std;
+
+namespace queries {
+class ComputeIncome : public ComputeQuery {
+public:
+    using ComputeQuery::ComputeQuery;
+    ReadResult Process(const BudgetManager& budget) const override {
+        return {budget.ComputeSum(GetFrom(), GetTo())};
     }
-    return string.substr(start, string.find_last_not_of(' ') + 1 - start);
-}
 
-/**
- * Разбивает строку string на n строк, с помощью указанного символа-разделителя delim
- */
-std::vector<std::string_view> Split(std::string_view string, char delim) {
-    std::vector<std::string_view> result;
-
-    size_t pos = 0;
-    while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
-        auto delim_pos = string.find(delim, pos);
-        if (delim_pos == string.npos) {
-            delim_pos = string.size();
+    class Factory : public QueryFactory {
+    public:
+        std::unique_ptr<Query> Construct(std::string_view config) const override {
+            auto parts = Split(config, ' ');
+            return std::make_unique<ComputeIncome>(Date::FromString(parts[0]), Date::FromString(parts[1]));
         }
-        if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
-            result.push_back(substr);
+    };
+};
+
+class Alter : public ModifyQuery {
+public:
+    Alter(Date from, Date to, double amount)
+        : ModifyQuery(from, to)
+        , amount_(amount) {
+    }
+
+    void Process(BudgetManager& budget) const override {
+        double day_income = amount_ / (Date::ComputeDistance(GetFrom(), GetTo()) + 1);
+
+        budget.AddBulkOperation(GetFrom(), GetTo(), BulkMoneyAdder{day_income});
+    }
+
+    class Factory : public QueryFactory {
+    public:
+        std::unique_ptr<Query> Construct(std::string_view config) const override {
+            auto parts = Split(config, ' ');
+            double payload = std::stod(std::string(parts[2]));
+            return std::make_unique<Alter>(Date::FromString(parts[0]), Date::FromString(parts[1]), payload);
         }
-        pos = delim_pos + 1;
+    };
+
+private:
+    double amount_;
+};
+
+class PayTax : public ModifyQuery {
+public:
+    using ModifyQuery::ModifyQuery;
+
+    void Process(BudgetManager& budget) const override {
+        budget.AddBulkOperation(GetFrom(), GetTo(), BulkTaxApplier{1});
     }
 
-    return result;
-}
+    class Factory : public QueryFactory {
+    public:
+        std::unique_ptr<Query> Construct(std::string_view config) const override {
+            auto parts = Split(config, ' ');
+            return std::make_unique<PayTax>(Date::FromString(parts[0]), Date::FromString(parts[1]));
+        }
+    };
 
-//Три вида запросов:
-//Earn 2000-01-02 2000-01-06 20
-//ComputeIncome 2000-01-01 2001-01-01
-//PayTax 2000-01-02 2000-01-03
+private:
+};
 
-CommandDescription ParseCommandDescription(std::string_view line) {
-    /*auto colon_pos = line.find(':');
-    if (colon_pos == line.npos) {
-        return {};
-    }*/
-    //std::string_view trimmed_line = Trim(line);
-    ////первый пробел - после команды
-    //auto space_command_pos = trimmed_line.find(' ');
-    //if (space_command_pos == line.npos) {
-    //    return {};
-    //}
-    ////второй пробел - после первой даты
-    //auto space_first_date_pos = trimmed_line.find(' ',space_command_pos);
-    //if (space_first_date_pos == line.npos) {
-    //    return {};
-    //}
-    ////(только для команды Earn) третий пробел - после второй даты
-    //auto space_second_date_pos = trimmed_line.find(' ', space_first_date_pos);
-    //if (space_second_date_pos == line.npos) {
-    //    return {};
-    //}
-    //auto not_space = line.find_first_not_of(' ', space_pos);
-    //if (not_space >= colon_pos) {
-    //    return {};
-    //}
-    auto request_strings = Split(line, ' ');
-    
-    if (request_strings.size() == 3) {
-        return{ std::string(request_strings[0]),
-            std::string(request_strings[1]),
-            std::string(request_strings[2]),
-            {}
-        };
-    }
+}  // namespace queries
 
-    if (request_strings.size() == 4) {
-        return{ std::string(request_strings[0]),
-            std::string(request_strings[1]),
-            std::string(request_strings[2]),
-            std::string(request_strings[3])
-        };
-    }
-    else return {};
-}
+const QueryFactory& QueryFactory::GetFactory(std::string_view id) {
+    static queries::ComputeIncome::Factory compute_income;
+    static queries::Alter::Factory earn;
+    static queries::PayTax::Factory pay_tax;
+    static std::unordered_map<std::string_view, const QueryFactory&> factories
+        = {{"ComputeIncome"sv, compute_income}, {"Earn"sv, earn}, {"PayTax"sv, pay_tax}};
 
-void Request::ParseLine(std::string_view line) {
-    command_description_ = ParseCommandDescription(line);
-}
-
-void Request::ApplyCommand(std::ostream* out) {
-    if (command_description_.command == EARN_REQUEST_STRING) {
-        manager_.SetEarnings(Date::FromString(command_description_.date_from),
-            Date::FromString(command_description_.date_to),
-            std::stod(command_description_.description));
-    }
-    else if (command_description_.command == COMPUTE_REQUEST_STRING) {
-        *out << manager_.GetEarnings(Date::FromString(command_description_.date_from),
-            Date::FromString(command_description_.date_to))<<std::endl;
-    }
-    else if (command_description_.command == PAY_TAX_REQUEST_STRING){
-        manager_.PayTax(Date::FromString(command_description_.date_from),
-            Date::FromString(command_description_.date_to),
-            std::stod(command_description_.description));
-    }
-    else manager_.SetSpendings(Date::FromString(command_description_.date_from),
-        Date::FromString(command_description_.date_to),
-        std::stod(command_description_.description));
+    return factories.at(id);
 }
